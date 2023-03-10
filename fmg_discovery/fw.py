171508da1a4b8e6ea18b4e8a7ea61882b6cc5355
andersh <anders.haal@ingby.com>
2023-03-10 13:28:35 +0100

    Add license

--- fmg_discovery/fw.py
+++ fmg_discovery/fw.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2023  Anders Håål
+
+    This file is part of fortigate-exporter-discovery.
+
+    fortigate-exporter-discovery is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    fortigate-exporter-discovery is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fortigate-exporter-discovery.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 from typing import Dict, Any
 
 
@@ -21,4 +42,3 @@ class Fortigate:
 
     def as_prometheus_file_sd_entry(self) -> Dict[str, Any]:
         return {'target': [f"https://{self.ip}:{self.port}"], 'labels': self.as_labels()}
-
