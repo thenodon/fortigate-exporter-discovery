f02b00553e929a1dca2feb317bd65bcfc0af81f4
andersh <anders.haal@ingby.com>
2023-03-10 13:48:45 +0100

    Add build and github actions

---
+++ setup.py
@@ -0,0 +1,39 @@
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+
+setup(
+    name='fortigate-exporter-discovery',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    setuptools_git_versioning={
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}",
+        "dirty_template": "{tag}.post{ccount}+git.{sha}.dirty",
+        "starting_version": "0.0.1",
+        "version_callback": None,
+        "version_file": None,
+        "count_commits_from_version_file": False,
+        "branch_formatter": None
+    },
+    setup_requires=['setuptools-git-versioning'],
+    packages=find_packages(),
+    author='thenodon',
+    author_email='aha@ingby.com',
+    url='https://github.com/thenodon/fortigate-exporter-discovery',
+    license='GPLv3',
+    include_package_data=True,
+    zip_safe=False,
+    description="A Prometheus file discovery for Fortigate's based on FortiManager",
+    install_requires=read('requirements.txt').split(),
+    python_requires='>=3.8',
+)
